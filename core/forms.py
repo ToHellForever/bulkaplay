a3dd4dbac4e4eb7ae9d514a3b45bf7e286726a53
ToHellForever <dim.anosoff2018@yandex.ru>
2025-09-12 16:20:17 +0300

    .форма

--- core/forms.py
+++ core/forms.py
@@ -4,15 +4,43 @@ from django.utils import timezone
 from datetime import datetime
 
 ORDER_TYPES = [
-    ('buy', 'Купить'),
-    ('rent', 'Аренда')
+    ('choice', 'Выбрать...'),
+    ('buy', 'Купить игру'),
+    ('rent', 'Заказать аренду')
 ]
 
 
 class OrderForm(forms.Form):
-    name = forms.CharField(label='Имя:', max_length=100)
-    phone = forms.CharField(label='Телефон:', max_length=15)
-    order_type = forms.ChoiceField(label='Что вас интересует?', choices=ORDER_TYPES, initial='buy')
-    products = forms.ModelMultipleChoiceField(queryset=Product.objects.all(), required=False, widget=forms.CheckboxSelectMultiple, label='Продукты:')
-    arenda = forms.ModelMultipleChoiceField(queryset=Arenda.objects.all(), required=False, widget=forms.CheckboxSelectMultiple, label='Предметы аренды:')
-    comment = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), label='Комментарий к заказу:', required=False)
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Имя'})
+    )
+    phone = forms.CharField(
+        max_length=15,
+        widget=forms.TextInput(attrs={'placeholder': 'Номр телефона'})
+    )
+    order_type = forms.ChoiceField(
+        label='Что вас интересует?',
+        choices=ORDER_TYPES,
+        initial='choice'
+    )
+    products = forms.ModelMultipleChoiceField(
+        queryset=Product.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        label='Продукты:'
+    )
+    arenda = forms.ModelMultipleChoiceField(
+        queryset=Arenda.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        label='Предметы аренды:'
+    )
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'rows': 1,
+            'placeholder': 'Комментарий к заказу...'
+        }),
+        label='Комментарий к заказу:',
+        required=False
+    )

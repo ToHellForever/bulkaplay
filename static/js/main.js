5803c148e9183c5105787c1e9fadf7a97be10761
ToHellForever <dim.anosoff2018@yandex.ru>
2025-10-06 18:59:52 +0300

    добавил автоматическуб прокрутку во всех каруселях

--- static/js/main.js
+++ static/js/main.js
@@ -25,6 +25,62 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+// для карусели аренды
+document.addEventListener('DOMContentLoaded', function() {
+  var carouselElement = document.getElementById('arendaCarousel');
+
+  if (!carouselElement) {
+    console.error('Карусель не найдена.');
+    return;
+  }
+
+  if (typeof bootstrap === 'undefined' || typeof bootstrap.Carousel === 'undefined') {
+    console.error('Библиотека Bootstrap не доступна.');
+    return;
+  }
+
+  var carousel = new bootstrap.Carousel(carouselElement, {
+    interval: 5000, 
+    wrap: true
+  });
+
+  carouselElement.addEventListener('slid.bs.carousel', function(event) {
+    if (carouselElement.children.length > 1 && event.to === carouselElement.children.length - 1) {
+      setTimeout(function() {
+        carousel.to(0);
+      }, 2000);
+    }
+  });
+});
+
+// для карусел новостей
+document.addEventListener('DOMContentLoaded', function() {
+  var carouselElement = document.getElementById('newsCarousel');
+
+  if (!carouselElement) {
+    console.error('Карусель не найдена.');
+    return;
+  }
+
+  if (typeof bootstrap === 'undefined' || typeof bootstrap.Carousel === 'undefined') {
+    console.error('Библиотека Bootstrap не доступна.');
+    return;
+  }
+
+  var carousel = new bootstrap.Carousel(carouselElement, {
+    interval: 5000,
+    wrap: true
+  });
+
+  carouselElement.addEventListener('slid.bs.carousel', function(event) {
+    if (carouselElement.children.length > 1 && event.to === carouselElement.children.length - 1) {
+      setTimeout(function() {
+        carousel.to(0);
+      }, 2000);
+    }
+  });
+});
+
 // Бургер-меню
 document.addEventListener('DOMContentLoaded', function() {
     const burgerIcon = document.getElementById('burgerIcon');

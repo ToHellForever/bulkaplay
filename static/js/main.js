cd595d81abf7d7c180f7e4fcc83b9026f202051e
ToHellForever <dim.anosoff2018@yandex.ru>
2025-10-08 21:59:26 +0300

    получилось сделать карусель для продуктов, осталось выровнять карточки товаров и разобраться с кастомными стрелками для перелистывания

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var carouselElement = document.getElementById('productCarousel');
-
-  if (!carouselElement) {
-    console.error('Карусель не найдена.');
-    return;
-  }
-
-  if (typeof bootstrap === 'undefined' || typeof bootstrap.Carousel === 'undefined') {
-    console.error('Библиотека Bootstrap не доступна.');
-    return;
-  }
-
-  var carousel = new bootstrap.Carousel(carouselElement, {
-    interval: 5000,
-    wrap: true
-  });
-
-  carouselElement.addEventListener('slid.bs.carousel', function(event) {
-    if (carouselElement.children.length > 1 && event.to === carouselElement.children.length - 1) {
-      setTimeout(function() {
-        carousel.to(0);
-      }, 2000);
-    }
-  });
-});
-
 // для карусели аренды
 document.addEventListener('DOMContentLoaded', function() {
   var carouselElement = document.getElementById('arendaCarousel');
